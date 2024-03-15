5c05d3f94bd2a54ab369fe77aac7b8bcdbec9013
mailfaruqee <ismailfaruqi7@gmail.com>
2024-03-16 05:43:09 +0700

    feat: first step to initiate my Address Book

---
+++ README.md
@@ -0,0 +1,3 @@
+# Address Book
+
+This is a simple Address Book app of my friends using HTML and JS only.
---
+++ index.js
@@ -0,0 +1,19 @@
+const contacts = [
+    {
+        id: 1,
+        fullName: "Sayyid Al Murtadho",
+        age: 22,
+        phone: "+6281234567890",
+        office: "Vriens and Partners"
+    },
+    {
+        id: 2,
+        fullName: "Wahyu Agung Primantaka",
+        age: 23,
+        phone: "+6280987654321",
+        office: "Antam"
+    }
+]
+
+console.log(contacts);
+console.log(contacts[0]);
