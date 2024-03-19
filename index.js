7a754d9b1a984561c4cd5543c4fd55779a491222
mailfaruqee <ismailfaruqi7@gmail.com>
2024-03-19 14:17:00 +0700

    feat: try if else

--- index.js
+++ index.js
@@ -12,8 +12,28 @@ const contacts = [
         age: 23,
         phone: "+6280987654321",
         office: "Antam"
-    }
-]
+    },
+    {
+        id: 3,
+        fullName: "Ammar",
+        age: 20,
+        phone: "+623216549870",
+        office: "UGM"
+    },
+];
 
-console.log(contacts);
-console.log(contacts[0]);
+for (let index = 0; index < contacts.length; index++) {
+    // const contact = contacts[index];
+    const {id, fullName, phone, age} = contacts[index];
+    // let ageCategory = "";
+    const ageCategory = age > 21 ? "has graduated" : "is still in college";
+
+    // if (contact.age > 21) {
+    //     ageCategory = "has graduated";
+    // } else {
+    //     ageCategory = "still struggle with undergrad final thesis"
+    // }
+    // console.log(`${contact.id}. ${contact.fullName} (${contact.phone}) ${ageCategory}`);
+    console.log(`${id}. ${fullName} (${phone}) ${ageCategory}`);
+
+}
